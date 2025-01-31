3b7a6d08afe85a97a64f6315c8d06c8f39a21221
Juan Pablo <juansaner01@gmail.com>
2025-01-31 18:59:55 -0500

    Primer commit

--- includes/config/database.php
+++ includes/config/database.php
@@ -1,7 +1,11 @@
 <?php
 
 function conectarDB() : mysqli {
-    $db = new mysqli('localhost', 'root', 'root', 'bienesraices_crud');
+    $db = new mysqli($_ENV['DB_HOST'], 
+    $_ENV['DB_USER'], 
+    $_ENV['DB_PASSWORD'], 
+    $_ENV['DB_DBNAME'],
+    $_ENV['DB_PORT']);
 
     if(!$db) {
         echo 'Error no se pudo conectar: ' . mysqli_connect_error();
