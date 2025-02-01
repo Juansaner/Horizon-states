d2eac5d009d162eb571f2d906f905737f085dd5a
Juan Pablo <juansaner01@gmail.com>
2025-01-31 20:47:36 -0500

    Actualización ruta autoload.php

--- includes/config/database.php
+++ includes/config/database.php
@@ -1,6 +1,6 @@
 <?php
 
-require_once __DIR__ . '/../vendor/autoload.php'; // Cargar Composer
+require_once __DIR__ . '/vendor/autoload.php'; // Cargar Composer
 
 $dotenv = Dotenv\Dotenv::createImmutable(__DIR__ . '/../');
 $dotenv->load();
