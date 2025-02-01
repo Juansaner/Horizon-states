a6f15172a187445dcfcd48899aba55ed92151fe6
Juan Pablo <juansaner01@gmail.com>
2025-01-31 19:47:59 -0500

    Eliminacion DB_PORT

--- includes/config/database.php
+++ includes/config/database.php
@@ -4,8 +4,7 @@ function conectarDB() : mysqli {
     $db = new mysqli($_ENV['DB_HOST'], 
     $_ENV['DB_USER'], 
     $_ENV['DB_PASSWORD'], 
-    $_ENV['DB_DBNAME'],
-    $_ENV['DB_PORT']);
+    $_ENV['DB_DBNAME']);
 
     if(!$db) {
         echo 'Error no se pudo conectar: ' . mysqli_connect_error();
