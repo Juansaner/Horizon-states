71c0a2597baec60a0f287875bb05914eb63489ba
Juan Pablo <juansaner01@gmail.com>
2025-01-31 22:34:41 -0500

    Actualizacion href logo

--- views/layout.php
+++ views/layout.php
@@ -28,7 +28,7 @@
         <header class="header <?php echo $inicio ? 'inicio' : ''; ?>">
             <div class="contenedor contenido-header">
                 <div class="barra">
-                    <a href="/index.php">
+                    <a href="/">
                         <img class="logo" src="../build/img/logo.png" alt="Logotipo de bienes raíces">
                     </a>
     
