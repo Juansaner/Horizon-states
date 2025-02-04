b1860fede4acea2a109c4408ae78d22169ee2e9a
Juan Pablo <juansaner01@gmail.com>
2025-02-04 09:53:26 -0500

    Actualización cambio de iconos menu

--- views/layout.php
+++ views/layout.php
@@ -17,6 +17,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Horizon States</title>
+    <link rel="icon" href="build/img/icono-logo.jpg" type="image/jpg">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet"
         integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
     <link rel="stylesheet" href="../build/css/app.css">
@@ -33,7 +34,7 @@
                     </a>
     
                     <button class="menu-mobile" aria-label="Abrir menú de navegación" aria-expanded="false" aria-controls="navegacion"><!-- icono menú hamburguesa -->
-                        <svg xmlns="http://www.w3.org/2000/svg" width="50" height="50" fill="white" class="bi bi-list"
+                        <svg xmlns="http://www.w3.org/2000/svg" width="70" height="70" fill="white" class="bi bi-list"
                             viewBox="0 0 16 16" aria-hidden="true">
                             <path fill-rule="evenodd" d="M2.5 12a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5m0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5m0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5" />
                         </svg>
@@ -43,10 +44,7 @@
                         <div class="container-closeBtn">
                             <p class="label-menu" aria-hidden="true">Menú</p>
                             <button class="close-menu" role="button" aria-label="Cerrar menú">
-                                <svg xmlns="http://www.w3.org/2000/svg" width="50" height="50" fill="currentColor"
-                                    class="bi bi-x" viewBox="0 0 16 16" aria-hidden="true">
-                                    <path d="M4.646 4.646a.5.5 0 0 1 .708 0L8 7.293l2.646-2.647a.5.5 0 0 1 .708.708L8.707 8l2.647 2.646a.5.5 0 0 1-.708.708L8 8.707l-2.646 2.647a.5.5 0 0 1-.708-.708L7.293 8 4.646 5.354a.5.5 0 0 1 0-.708" />
-                                </svg>
+                                <img src="build/img/x-lg.svg" alt="icono cerrar menu" width="30" height="30">
                             </button>
                         </div>
                         <!--- .container-closeBtn --->
